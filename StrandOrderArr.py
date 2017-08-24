acbb214c8ba2c4d72e891da8a22dc9185a94efae
MichaelTonyFerrari <ferraro2ndbase@gmail.com>
2017-08-24 07:41:57 -0400

    Create structured array, doesnt modify orginial array

--- StrandOrderArr.py
+++ StrandOrderArr.py
@@ -12,3 +12,11 @@ ppl_array[3] = ('Delta', 71, 215, 23)
 ppl_array[0] = ('Alpha', 43, 173, 40)
 
 print(ppl_array)
+print(ppl_array[0:])
+
+ages = ppl_array['age']
+
+print(ages)
+
+make_younger = ages/2
+print(make_younger)
